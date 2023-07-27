6bbeb54396fbbe4a239a2c8ce4b2bfbf60aacb7e
Destello99 <64770972+Destello99@users.noreply.github.com>
2023-07-27 21:57:04 +0530

    Add files via upload

---
+++ ShiftZero.java
@@ -0,0 +1,10 @@
+import java.lang.reflect.Array;
+
+public class ShiftZero {
+
+    public static void main(String[] args) {
+        int arr[] = {0,1,0,3,1,2};
+        System.out.print(arr);
+    }
+    
+}
