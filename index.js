3aea09ed1c2578cc79dc0b61934d4e3fdad47b32
Destello99 <pokego9175@gmail.com>
2023-07-28 01:47:27 +0530

    deleted work

--- index.js
+++ index.js
@@ -24,13 +24,22 @@ app.listen(5000, ()=>{
 })
 
 app.post("/add", (req, res)=>{
-    const {sname, email, phone} = req.body;
-    const sqlInsert = "insert into student (sname, email,phone) values(?,?,?)";
-    db.query(sqlInsert, [sname,email,phone], (err, result)=>{
+    const {name, email, phone} = req.body;
+    const sqlInsert = "insert into student (name, email,phone) values(?,?,?)";
+    db.query(sqlInsert, [name,email,phone], (err, result)=>{
         err ? console.log(err) : console.log("wow");
     });
 });
 
+app.delete("/delete/:id", (req , res)=>{
+    const {id} = req.params;
+    const sqlRemove = "delete from student where id = ?";
+    db.query(sqlRemove, id, (error, result)=>{
+        if(error){
+            console.log(error)
+        }
+    });
+});
 app.get("/get", (req, res)=>{
     const getdata = "select * from student";
     db.query(getdata, (err, result)=>{
